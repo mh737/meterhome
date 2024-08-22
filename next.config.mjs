97099f6760aba2086990da6a92a23f0abdbc0260
Your Name <you@example.com>
2024-08-22 21:23:05 +0800

    new1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
+ 
  
 export default withNextIntl(nextConfig);
