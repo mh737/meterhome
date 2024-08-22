71dd1e070570b0ac5a7336a3424ffce4a2656b88
Your Name <you@example.com>
2024-08-22 23:20:09 +0800

    new1

--- src/app/[locale]/home/HomeP.jsx
+++ src/app/[locale]/home/HomeP.jsx
@@ -13,7 +13,7 @@ function HomeP() {
 
       <div className="home ">
         <div className="meterfore">
-          <h1 className="meter w-[100vw] md:w-fit">METER</h1>
+          <h1 className="meter w-[100vw] md:w-fit text-center">METER</h1>
 
         </div> 
         <div className="flex-col flex align-middle flex-wrap">
