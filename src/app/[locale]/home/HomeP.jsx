320bb525cec4cf8d7b9891798a4288fc2333ba87
Shalev <admin@shalev.eu.org>
2024-09-10 05:40:02 +0000

    english done

--- src/app/[locale]/home/HomeP.jsx
+++ src/app/[locale]/home/HomeP.jsx
@@ -4,27 +4,26 @@ import { useTranslations } from "next-intl";
 import { useState, useEffect } from "react";
 
 import React from "react";
-import Popup from 'reactjs-popup';
+import Popup from "reactjs-popup";
 import "./popup.css";
 
 import "./home.css";
 import Image from "next/image";
 import { Link as NavLink } from "../../../navigation";
 
-import Image1 from "../../../../public/image/Demo2/1.png"
-import Image2 from "../../../../public/image/Demo2/2.png"
-import Image3 from "../../../../public/image/Demo2/3.png"
-import Image4 from "../../../../public/image/Demo2/4.png"
-import Image5 from "../../../../public/image/Demo2/5.png"
-import Image6 from "../../../../public/image/Demo2/6.png"
-import Image7 from "../../../../public/image/Demo2/7.png"
-import Image8 from "../../../../public/image/Demo2/8.png"
-import Image9 from "../../../../public/image/Demo2/9.png"
-import Image10 from "../../../../public/image/Demo2/10.jpg"
-
+import Image1 from "../../../../public/image/Demo2/1.png";
+import Image2 from "../../../../public/image/Demo2/2.png";
+import Image3 from "../../../../public/image/Demo2/3.png";
+import Image4 from "../../../../public/image/Demo2/4.png";
+import Image5 from "../../../../public/image/Demo2/5.png";
+import Image6 from "../../../../public/image/Demo2/6.png";
+import Image7 from "../../../../public/image/Demo2/7.png";
+import Image8 from "../../../../public/image/Demo2/8.png";
+import Image9 from "../../../../public/image/Demo2/9.png";
+import Image10 from "../../../../public/image/Demo2/10.jpg";
 
 function HomeP() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  // const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   const t = useTranslations("HomePage");
   const [mounted, setMounted] = useState(false);
@@ -36,62 +35,104 @@ function HomeP() {
   return (
     <>
       <script src="/nav.js" async />
-      <Popup trigger={<button className="trigger-button">Click to open popup</button>} position="right center" modal nested>
-        {close => (
-          <div className="modal">
-            <button className="close" onClick={close}>
-              &times;
-            </button>
-            <div className="header"> GeeksforGeeks </div>
-            <div className="content">
-              This is a simple popup example.
-            </div>
-            <div className="actions">
-              <button className="button" onClick={() => {
-                console.log('Button clicked');
-                close();
-              }}>Click here</button>
-            </div>
-          </div>
-        )}
-      </Popup>
+
       <div className="w-full md:text-center items-center md:wrap md:flex flex-col">
         <div className="ml-5 !z-10 !relative max-h-[348px]">
-        <h1 className="text-7xl mb-4 md:mt-32 mt-4">Ultra-Wide Band eID Validation System</h1>
-        <Nextbut onPress={onOpen} className="p-2 border-amber-50 border-[1px] bg-black">Demostration</Nextbut>
+          <h1 className="text-7xl mb-4 md:mt-32 mt-4">
+            Ultra-Wide Band eID Validation System
+          </h1>
+
+          <Popup
+            trigger={
+              <button className="p-2 border-amber-50 border-[1px] bg-black">
+                Demostration
+              </button>
+            }
+            position="right center"
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                {/* <button className="close" onClick={close}>
+                  &times;
+                </button> */}
+                {/* <div className="header"> GeeksforGeeks </div> */}
+                <div className="content">
+                  <video
+                    width="320"
+                    height="240"
+                    controls
+                    preload="true"
+                    autoplay playsinline muted loop
+                  >
+                    <source src="/image/Demo2/ani1.MP4" type="video/mp4" />
+                    <track
+                      src="/path/to/captions.vtt"
+                      kind="subtitles"
+                      srcLang="en"
+                      label="English"
+                    />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+                <div className="actions">
+                  {/* <button
+                    className="button"
+                    onClick={() => {
+                      console.log("Button clicked");
+                      close();
+                    }}
+                  >
+                    Click here
+                  </button> */}
+                </div>
+              </div>
+            )}
+          </Popup>
+        </div>
+        <Image
+          src={Image1}
+          alt="Vercel Logo"
+          className="large-pic !relative mt-[-120px] !z-0"
+          priority
+        />
+        <div className="w-[70%] flex ml-auto mr-auto justify-between en mt-[-50px] z-10 !relative xl:w-[50%] md:mt-[-100px]">
+          <div className="text-2xl">METER portal</div>
+          <div className="text-2xl">Your device</div>
+        </div>
       </div>
-      <Image
-        src={Image1}
-        alt="Vercel Logo"
-        className="large-pic !relative mt-[-120px] !z-0"
-        priority
-      />
-      <div className="w-[70%] flex ml-auto mr-auto justify-between en mt-[-50px] z-10 !relative xl:w-[50%] md:mt-[-100px]">
-        <div className="text-2xl">METER portal</div>
-        <div className="text-2xl">Your device</div>
-      </div>
-      </div>
-      
-  
+
       <div className="unitwrapper spacer-top !flex-nowrap">
         <h2 className="eyebrow block">Feature</h2>
         <p className="headline block"> Revolutionary eID system</p>
         <div className="section-content text-center">
           {/* <p className="reduced-headline"></p> */}
           <p className="section-body  w-[100%] md:w-[80%] ml-auto mr-auto">
-            Through METER portal, you can instantly issue and revoke eID, anywhere, anytime.
+            Through METER portal, you can instantly issue and revoke eID,
+            anywhere, anytime.
           </p>
         </div>
         <div className="w-[200%] md:w-full overflow-hidden text-center">
-          <Image src={Image2} alt="Vercel Logo" className="max-w-[50%] inline-block md:h-[100vh] w-auto" priority />
-          <Image src={Image3} alt="Vercel Logo" className="max-w-[50%] inline-block md:h-[100vh] w-auto" priority />
-
+          <Image
+            src={Image2}
+            alt="Vercel Logo"
+            className="max-w-[50%] inline-block md:h-[100vh] w-auto"
+            priority
+          />
+          <Image
+            src={Image3}
+            alt="Vercel Logo"
+            className="max-w-[50%] inline-block md:h-[100vh] w-auto"
+            priority
+          />
         </div>
-        <p className="headline block"> True contactless expreience</p>
+        <p className="headline block pt-28"> True contactless expreience</p>
         <div className="section-content text-center">
           {/* <p className="reduced-headline"></p> */}
           <p className="section-body  w-[100%] md:w-[80%] ml-auto mr-auto pb-20">
-            The anchor automatically connects to your device. <br /> Approach it to free your hands
+            The anchor automatically connects to your device. <br /> Approach it
+            to free your hands
           </p>
         </div>
 
@@ -101,18 +142,32 @@ function HomeP() {
         <p className="headline block pb-7 pt-28">{t("extra-security")}</p>
 
         <div className="section-content text-center">
-          <p className="reduced-headline"> Your e-ID is stored on the Secure Element on your phone</p>
+          <p className="reduced-headline">
+            {" "}
+            Your e-ID is stored on the Secure Element on your phone
+          </p>
 
-          <Image src={Image5} alt="Vercel Logo" className="h-[200px] mr-auto ml-auto mt-10 w-[200px]" priority />
+          <Image
+            src={Image5}
+            alt="Vercel Logo"
+            className="h-[200px] mr-auto ml-auto mt-10 w-[200px]"
+            priority
+          />
           <p className="section-body  w-[100%] md:w-[80%] ml-auto mr-auto">
-            Ultra-Wide Band technology (UWB) enables real-time ranging to provide tamper-resistant protection
+            Ultra-Wide Band technology (UWB) enables real-time ranging to
+            provide tamper-resistant protection
           </p>
         </div>
         <div className="speakers-container">
           <Image src={Image6} alt="Vercel Logo" className="largepic" priority />
         </div>
         <div className="w-full flex wrap flex-col md:flex-row md:w-[80vw] md:justify-center items-center">
-          <Image src={Image7} alt="Vercel Logo" className="md:h-[100vh] md:w-auto w-[70%] inline-block" priority />
+          <Image
+            src={Image7}
+            alt="Vercel Logo"
+            className="md:h-[100vh] md:w-auto w-[70%] inline-block"
+            priority
+          />
           <div className="section-content text-center md:!w-[50%] !inline-block">
             <p className="reduced-headline">Lost mode</p>
             <p className="section-body  w-[100%] md:w-[80%] ml-auto mr-auto">
@@ -120,7 +175,6 @@ function HomeP() {
             </p>
           </div>
         </div>
-
       </div>
       <div className="unitwrapper spacer-top !flex-nowrap !bg-slate-100 !text-slate-950">
         <h2 className="eyebrow block">Use Case</h2>
@@ -128,61 +182,124 @@ function HomeP() {
         <div className="flex flex-wrap w-full md:!w-[80%]">
           <Image src={Image8} alt="Vercel Logo" className="largepic" priority />
           <p className="section-body spacer-bottom w-[100%] md:w-[80%] ml-auto mr-auto text-center">
-            Parking entrance and exit. No more annoying stopping.<br /><br />
-            <button className="p-3 bg-orange-500 rounded-full font-thin"> <div className="rounded-full border-white border-2 inline-block w-8 text-white">→</div> Learn more about <div className="font-bold inline-block hover:text-white ">Parking</div></button>
-            {/* <button className="p-3 bg-orange-500  rounded-full hover:text-white">→ Learn more about <b>Parking</b></button> */}
+            Parking entrance and exit. No more annoying stopping.
+            <br />
+            <br />
+            <button className="p-3 bg-orange-500 rounded-full font-thin goto">
+              {" "}
+              <div className="rounded-full border-white border-2 inline-block w-9 text-white">
+                <div className="w-full h-full flex items-center justify-center">
+                  →
+                </div>
+              </div>{" "}
+              Learn more about{" "}
+              <div className="font-bold inline-block hover:text-white servicegoto ">
+                Parking
+              </div>
+            </button>
           </p>
 
           <Image src={Image9} alt="Vercel Logo" className="largepic" priority />
           <p className="section-body spacer-bottom w-[100%] md:w-[80%] ml-auto mr-auto text-center">
-            Access control at different types of properties. You can share a one-time pass with your guest<br /><br />
-            <button className="p-3 bg-orange-500 rounded-full font-thin"> <div className="rounded-full border-white border-2 inline-block w-8 text-white">→</div> Learn more about <div className="font-bold inline-block hover:text-white ">Access control</div></button>
-            {/* <button className="p-3 bg-orange-500  rounded-full hover:text-white">→ Learn more about <b>Access control</b></button> */}
+            Access control at different types of properties. You can share a
+            one-time pass with your guest
+            <br />
+            <br />
+            <button className="p-3 bg-orange-500 rounded-full font-thin goto">
+              {" "}
+              <div className="rounded-full border-white border-2 inline-block w-9 text-white">
+                <div className="w-full h-full flex items-center justify-center">
+                  →
+                </div>
+              </div>{" "}
+              Learn more about{" "}
+              <div className="font-bold inline-block hover:text-white  servicegoto">
+                Access-control
+              </div>
+            </button>
           </p>
-          <Image src={Image10} alt="Vercel Logo" className="largepic" priority />
+          <Image
+            src={Image10}
+            alt="Vercel Logo"
+            className="largepic"
+            priority
+          />
           <p className="section-body spacer-bottom w-[100%] md:w-[80%] ml-auto mr-auto text-center">
-            Integrated into self-service kiosk. You can now truly focus on your work <br /><br />
-            <button className="p-3 bg-orange-500 rounded-full font-thin"> <div className="rounded-full border-white border-2 inline-block w-8 text-white">→</div> Learn more about <div className="font-bold inline-block hover:text-white ">Self-service</div></button>
+            Integrated into self-service kiosk. You can now truly focus on your
+            work <br />
+            <br />
+            <button className="p-3 bg-orange-500 rounded-full font-thin goto">
+              {" "}
+              <div className="rounded-full border-white border-2 inline-block w-9 text-white">
+                <div className="w-full h-full flex items-center justify-center">
+                  →
+                </div>
+              </div>{" "}
+              Learn more about{" "}
+              <div className="font-bold inline-block hover:text-white servicegoto">
+                Self-service
+              </div>
+            </button>
           </p>
         </div>
       </div>
       <div className=" !bg-slate-950 !text-slate-100">
-        
         <div className="w-full md:w-[90%] flex ml-auto mr-auto mt-32 ">
           <div className="w-[50%]">
-          <div className="text-slate-300">Contact</div>
-              <ul>
-                <NavLink href="/"><li className="text-2xl">Find US</li></NavLink>
-                <NavLink href="/"><li className="text-2xl">General Enquiry</li></NavLink>
-                <NavLink href="/"><li className="text-2xl">Feedback</li></NavLink>
-                <NavLink href="/"><li className="text-2xl">Request a Test Ride</li></NavLink>
-              </ul>
+            <div className="text-slate-300">Contact</div>
+            <ul>
+              <NavLink href="/">
+                <li className="text-2xl">Find US</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl">General Enquiry</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl">Feedback</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl">Request a Test Ride</li>
+              </NavLink>
+            </ul>
           </div>
           <div className="w-[50%]">
-              <div className="text-slate-300">Explore METER</div>
-              <ul>
-                <NavLink href="/"><li className="text-2xl">METER HK Offcial Online Store</li></NavLink>
-                <NavLink href="/"><li className="text-2xl">Subscribe to our newsletter</li></NavLink>
-                <NavLink href="/"><li className="text-2xl">METERLIMITED.com</li></NavLink>
-              </ul>
+            <div className="text-slate-300">Explore METER</div>
+            <ul>
+              <NavLink href="/">
+                <li className="text-2xl">METER HK Offcial Online Store</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl">Subscribe to our newsletter</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl">METERLIMITED.com</li>
+              </NavLink>
+            </ul>
           </div>
         </div>
         <div className="w-full md:w-[90%] flex ml-auto mr-auto mt-32 mb-32">
-        <div className="w-[50%]">
-        <ul>
-                <NavLink href="/"><li className="text-2xl inline-block mr-3">FB</li></NavLink>
-                <NavLink href="/"><li className="text-2xl inline-block">IG</li></NavLink>
-              </ul>
-        </div>
-        <div className="w-[50%]">
-        <ul className="text-end">
-                <NavLink href="/"><li className="text-2xl inline-block mr-3">English</li></NavLink>
-                <NavLink href="/"><li className="text-2xl inline-block">Chinese</li></NavLink>
-              </ul>
-        </div>
+          <div className="w-[50%]">
+            <ul>
+              <NavLink href="/">
+                <li className="text-2xl inline-block mr-3">FB</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl inline-block">IG</li>
+              </NavLink>
+            </ul>
+          </div>
+          <div className="w-[50%]">
+            <ul className="text-end">
+              <NavLink href="/">
+                <li className="text-2xl inline-block mr-3">English</li>
+              </NavLink>
+              <NavLink href="/">
+                <li className="text-2xl inline-block">Chinese</li>
+              </NavLink>
+            </ul>
+          </div>
         </div>
       </div>
-
     </>
   );
 }
