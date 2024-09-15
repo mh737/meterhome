62e0b8cc5f3f407a9247ff8eac78d484e34af584
Your Name <you@example.com>
2024-09-15 14:58:10 +0100

    new1

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,10 +2,16 @@ import { NextResponse, type NextRequest } from 'next/server'
 import  excuteQuery  from "./db.js";
 
 
-export async function POST(){
-  return NextResponse.json({
-    hello:"world",
-  })
+export async function POST(request: Request , {params}: any) {
+  
+  const formData = await request.formData()
+  const name = formData.get('query')
+  const email = formData.get('value')
+  const result = await excuteQuery({
+    query: name,
+    values: email,
+  });
+    return new Response("ttt" + result , {      status: 200,    })
 }
 
 export async function GET(request: NextRequest, context: any) {
@@ -14,22 +20,14 @@ export async function GET(request: NextRequest, context: any) {
   if(typeof(result[0]['LAST_INSERT_ID()'])=== undefined){
     return new Response('Database not connected!', {      status: 400,    })}
   else{
-  return new Response('Hello, Next.js!', {      status: 200,    })}
+  return new Response('Connected to database!'+ result[0]['LAST_INSERT_ID()'], {      status: 200,    })}
 };
 
 // import {excuteQuery1} from '../db.js' //import {actual function} 
 // import db from "../db.js" //import default function
 // import excuteQuery from '../../db.js'
-// export async function POST(request: Request , {params}: any) {
+// 
 //   try{
-//   const formData = await request.formData()
-//   const name = formData.get('name')
-//   const email = formData.get('email')
-//        const result = await excuteQuery({
-//         query: 'INSERT INTO user(content) VALUES(?)',
-//         values: [params.userID],
-//     });
-//     console.log( "ttt",result );
 //   return Response.json({ name, email })}
 //   catch(e){console.log(e)}
 // }
