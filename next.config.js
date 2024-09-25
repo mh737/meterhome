7d942ef85ffb31e4681a3d91b30419e1ca327e15
Shalev <admin@shalev.eu.org>
2024-09-25 19:20:49 +0000

    new i18n

--- next.config.js
+++ next.config.js
@@ -3,21 +3,23 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // webpack5: false,  //wepack4
+    future: {
+      webpack5: true,
+    },
+    webpack: (config, { isServer }) => {
+      // if (!isServer) {
+        config.optimization = {
+          ...config.optimization,
+          minimize: false,
+        };
+      // }
+      return config;
+    }
+  };
  
-module.exports = withNextIntl(nextConfig)
-,{
-  // webpack5: false,  //wepack4
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { isServer }) => {
-    // if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: false,
-      };
-    // }
-    return config;
-  }
-};
+module.exports =  withNextIntl(nextConfig)
+
+https://stackoverflow.com/questions/67165873/how-do-i-combine-several-module-exports-in-next-js-next-config-js
+  ;
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -8,25 +8,25 @@ export const routing = defineRouting({
   // Used when no locale matches
   defaultLocale: 'hk',
   localePrefix: 'always',
-  domains: [
-    {
-        domain: 'www.meterlimited.com',
-        defaultLocale: 'hk'
-      // If there are no `locales` specified on a domain,
-      // all available locales will be supported here
+  // domains: [
+  //   {
+  //       domain: 'www.meterlimited.com',
+  //       defaultLocale: 'hk'
+  //     // If there are no `locales` specified on a domain,
+  //     // all available locales will be supported here
    
-      },
-    {
-      domain: 'hk.meterlimited.com',
-      defaultLocale: 'hk',
-      // Optionally restrict the locales available on this domain
-      locales: ['en','hk']
-    },
-    {
-      domain: 'uk.meterlimited.com',
-      defaultLocale: 'en',
-      locales: ['en'] }
-  ]
+  //     },
+  //   {
+  //     domain: 'hk.meterlimited.com',
+  //     defaultLocale: 'hk',
+  //     // Optionally restrict the locales available on this domain
+  //     locales: ['en','hk']
+  //   },
+  //   {
+  //     domain: 'uk.meterlimited.com',
+  //     defaultLocale: 'en',
+  //     locales: ['en'] }
+  // ]
 });
  
 // Lightweight wrappers around Next.js' navigation APIs
