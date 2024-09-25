14f3c1ec40f991144d88347862d71add95b74a2b
Shalev <admin@shalev.eu.org>
2024-09-25 18:12:35 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,19 +8,19 @@ const nextConfig = {};
 module.exports = withNextIntl(nextConfig);
 
 
-  // next.config.js
-module.exports = {
-  // webpack5: false,  //wepack4
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { isServer }) => {
-    // if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: false,
-      };
-    // }
-    return config;
-  }
-};
+//   // next.config.js
+// module.exports = {
+//   // webpack5: false,  //wepack4
+//   future: {
+//     webpack5: true,
+//   },
+//   webpack: (config, { isServer }) => {
+//     // if (!isServer) {
+//       config.optimization = {
+//         ...config.optimization,
+//         minimize: false,
+//       };
+//     // }
+//     return config;
+//   }
+// };
