318f49192398290896cb1955d737b6677282ac25
Shalev <admin@shalev.eu.org>
2024-09-25 16:09:44 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+ 
+module.exports = withNextIntl(nextConfig);
+
 
   // next.config.js
 module.exports = {
