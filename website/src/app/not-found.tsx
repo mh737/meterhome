9c1d75b4c4a807b7010a848f6d662d7db58b341e
Shalev <admin@shalev.eu.org>
2024-10-20 19:20:20 +0100

    Update not-found.tsx

--- website/src/app/not-found.tsx
+++ website/src/app/not-found.tsx
@@ -14,6 +14,34 @@
 import Error from 'next/error';
  
 export default function NotFound() {
+
+      var PATH_SEGMENTS_TO_SKIP = 1;
+
+      /**
+       * DO NOT TOUCH ANYTHING BELOW THIS COMMENT UNLESS YOU KNOW WHAT YOU ARE DOING
+       */
+      function isUrl(url: string) {
+        // Regex from https://stackoverflow.com/a/3809435, with a modification to allow for TLDs of up to 24 characters
+        return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,24}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)+$/.test(
+          url
+        );
+      }
+
+      function redirect() {
+        var location = window.location;
+        var issueNumber = location.pathname.split("/")[
+          PATH_SEGMENTS_TO_SKIP + 1  //how many segments to skip
+        ];
+        var homepage =
+          location.protocol +
+          "//" +
+          location.hostname +
+          (location.port ? ":" + location.port : "") +
+          "/" +
+          location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
+       location.replace(homepage);
+      }
+ redirect();
   return (
     <html lang="en">
       <body>
